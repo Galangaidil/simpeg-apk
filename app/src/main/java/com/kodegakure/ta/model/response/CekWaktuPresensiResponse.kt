f2b39997bbdbe67257ba68966cd6b9a4d9eb7438
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-02-26 12:26:25 +0700

    Menambahkan response jam dan tanggal

--- app/src/main/java/com/kodegakure/ta/model/response/CekWaktuPresensiResponse.kt
+++ app/src/main/java/com/kodegakure/ta/model/response/CekWaktuPresensiResponse.kt
@@ -3,5 +3,7 @@ package com.kodegakure.ta.model.response
 data class CekWaktuPresensiResponse(
     val keterangan: String,
     val button_text: String,
-    val endpoint: String
+    val endpoint: String,
+    val jam: String,
+    val tanggal: String
 )
